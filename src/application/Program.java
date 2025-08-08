8210b40a24cc84ded4f3be96421ebdc6fed1ca3e
Hiran Ferreti Baccos <hiranfbcj@outlook.com>
2025-08-08 19:57:52 -0300

    first commit

---
+++ src/application/Program.java
@@ -0,0 +1,24 @@
+package application;
+
+import java.util.List;
+import java.util.ArrayList;
+
+import entities.Product;
+
+public class Program {
+	public static void main(String[] args) {
+		List<Product> list = new ArrayList<>();
+
+		list.add(new Product("Tv", 900.00));
+		list.add(new Product("Mouse", 50.00));
+		list.add(new Product("Tablet", 350.00));
+		list.add(new Product("HD Case", 80.90));
+
+		double min = 100.0;
+		list.removeIf(p -> p.getPrice() >= min);
+
+		for (Product p : list) {
+			System.out.println(p);
+		}
+	}
+}
